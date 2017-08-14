85182ddd2aee348e718f449b6957a71387707d9e
Алексей Романовский <rav@tochka.com>
2017-08-14 10:33:22 +0500

    codeplex files import
    https://crmmodelgenerator.codeplex.com/

---
+++ CRM.Model.Generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+#region  == Copyright == 
+// =====================================================================
+//  Microsoft Consulting Services France - Aymeric Mouillé - 2013
+//  Projet     : CRM.Model.Generator - CRM.Model.Generator
+//  Fichier    : AssemblyInfo.cs (01/05/2013 13:18)
+//  
+//  Copyright (C) Microsoft Corporation.  All rights reserved.
+// =====================================================================
+#endregion
+
+#region  == Using == 
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly : AssemblyTitle("CRM.Model.Generator")]
+[assembly : AssemblyDescription("CRM Model Generator allow developers to generate entities classes for late binding development.")]
+[assembly : AssemblyConfiguration("")]
+[assembly : AssemblyCompany("Microsoft Consulting Services France - Aymeric Mouillé")]
+[assembly : AssemblyProduct("CRM.Model.Generator")]
+[assembly : AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly : AssemblyTrademark("")]
+[assembly : AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly : ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly : Guid("11dff122-337a-410f-9fa6-1e3e4ca52b1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly : AssemblyVersion("2015.04.23.1429")]
+[assembly: AssemblyFileVersion("2015.04.23.1429")]
